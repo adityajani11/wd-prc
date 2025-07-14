7b718a980257dd0523ba763873388d0cb33edaa4
Aditya Jani <67045611+adityajani11@users.noreply.github.com>
2025-07-14 11:11:10 +0530

    Added Interview Questions

---
+++ Interview.js
@@ -0,0 +1,16 @@
+console.log([] == ![]); // true
+// [](Treat as an object) == "" == 0
+// ![] == 0
+
+console.log([] == []); // false
+// [] == checks reference == ex. 354684
+// [] == checks reference == ex. 587465
+
+console.log(typeof NaN); // number
+console.log(typeof null); // object
+console.log(typeof object); // undefined
+console.log(typeof undefined); // undefined
+console.log("4" + 4); // Concatinates -> 44
+console.log("4" - 4); // 0
+console.log("4" * 4); // 16
+console.log("4" / 4); // 1
